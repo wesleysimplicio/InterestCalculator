9c2285198b5a0ef99df6785038c4e6efd20b5bdb
Wesley Simplicio <wesleysimplicio@live.com>
2021-01-18 03:16:39 -0300

    feat(API1): Applied swagger in project;

--- API1/API1/Controllers/TaxaJurosController.cs
+++ API1/API1/Controllers/TaxaJurosController.cs
@@ -7,10 +7,15 @@ using System.Threading.Tasks;
 namespace API1.Controllers
 {
     [ApiController]
-    [Route("api1/[controller]")]
+    [Route("[controller]")]
     public class TaxaJurosController : Controller
     {
-        [HttpGet("taxaJuros")]
+        /// <summary>
+        /// Retorna 0.01
+        /// </summary>
+        /// <returns>Valor 0.01</returns>
+        /// <response code="200">Retorna 0.01</response>
+        [HttpGet("/taxaJuros")]
         public IActionResult Get()
         {
             double resul = 0.01;
