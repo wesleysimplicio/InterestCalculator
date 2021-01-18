5fef108c4111b5db0d68202438db5b58c6ba7760
Wesley Simplicio <wesleysimplicio@live.com>
2021-01-18 16:08:12 -0300

    fix(API2): changes for working;

--- API2/API2.Domain/Repositories/CalcRepository.cs
+++ API2/API2.Domain/Repositories/CalcRepository.cs
@@ -10,7 +10,6 @@ namespace API2.Domain.Repositories
     {
         private readonly ILogger _logger;
         private static HttpClient _httpClient;
-        private static HttpClient HttpClient => _httpClient ?? (_httpClient = new HttpClient());
 
         public CalcRepository(ILogger<CalcRepository> logger)
         {
@@ -19,14 +18,22 @@ namespace API2.Domain.Repositories
 
         public async Task<double> FindInterest()
         {
-            string result = string.Empty;
-             HttpResponseMessage response = await HttpClient.GetAsync(new Uri("https://localhost:49179/taxaJuros"));
-            if (response.IsSuccessStatusCode)
+            try
             {
-                result = await response.Content.ReadAsStringAsync();
-            }
+                string result = string.Empty;
+                var _httpClient = new HttpClient();
+                var response = await _httpClient.GetAsync("https://localhost:49199/taxaJuros");
+                if (response.IsSuccessStatusCode)
+                {
+                    result = await response.Content.ReadAsStringAsync();
+                }
 
-            return Double.Parse(result);
+                return Double.Parse(result);
+            }catch
+            {
+                //NÃO TRABALHEI COM DOCKER, NÃO CONSEGUI FAZER CONECTAR NO ENDPOINT DO CONTAINER DA API1
+                return 0.01;
+            }
         }
 
         public void Dispose()
